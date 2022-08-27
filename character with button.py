c366b256d5e70573c782461239f07970d4a77ec1
josh <joshuazli2020@gmail.com>
2022-08-27 11:44:12 +1000

    reversed numbers happiness

--- character with button.py
+++ character with button.py
@@ -104,21 +104,21 @@ class Character():
 
     #happiness methods
 
-    def get_happiness(self):
+    def get_happiness(self):  
 
         #for warren, 0-5 from no happines to top happiness
         if self.happiness < 100/6:
-            return 0
-        elif self.happiness >= 100/6 and self.happiness < 200/6:
-            return 1
-        elif self.happiness >= 200/6 and self.happiness < 300/6:
-            return 2
-        elif self.happiness >= 300/6 and self.happiness < 400/6:
-            return 3
-        elif self.happiness >= 400/6 and self.happiness < 500/6:
-            return 4
-        else:
             return 5
+        elif self.happiness >= 100/6 and self.happiness < 200/6:
+            return 4
+        elif self.happiness >= 200/6 and self.happiness < 300/6:
+            return 3
+        elif self.happiness >= 300/6 and self.happiness < 400/6:
+            return 2
+        elif self.happiness >= 400/6 and self.happiness < 500/6:
+            return 1
+        else:
+            return 0
 
     def str_happiness(self):
         return str(self.happiness)
