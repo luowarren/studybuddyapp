4e1e7308826a5d360a8c306d9198cb8beaaacdd7
boomer3boom <109400623+boomer3boom@users.noreply.github.com>
2022-08-27 14:09:19 +1000

    Update Store_logic.py
    Store logic

--- Store_logic.py
+++ Store_logic.py
@@ -1,6 +1,8 @@
 """
 The logic behind transaction
 """
+from operator import itemgetter
+from typing_extensions import Self
 import pygame
 
 from Store_constants import *
@@ -11,22 +13,63 @@ class store_logic():
     Handle the logical stuff that's happening after buying items.
     """
 
-    def buy(Coin, Inventory):
+    def __init__(self, Inventory, Coin, Store_Item) -> None:
+        #Player's inventory
+        Self.Inventory = Inventory
+
+        #Player's available Coin
+        Self.Coin = Coin
+
+        #The item player still needs to buy
+        Self.Store_Item = Store_Item
+
+    def attempt_buy(Self, Item: ID):
         """
         Should determine if player can buy item. If player can buy the item,
         then add it into inventory and deduct the cost, otherwise tell player 
         they can't buy the item.
         """
-    
-    def remove_item(List): 
+
+        #In the end, just return whatever the player can still buy.
+
+        #If Item is already in player's inventory
+        if Item not in Self.Store_Item.get_store():
+            return self.list_item()
+
+        #If player does not have enough money
+        elif CLOTH_COST[Item] >= Self.Coin:
+            return self.list_item()
+
+        #Buy the item otherwise
+        else:
+            item_ID = self.Store_Item.get_store()
+            Self.deduct_amount(CLOTH_COST[Item])
+            Self.remove_item(Item)
+            Self.Inventory.add_item(Item)
+            return self.list_item()
+
+    def remove_item(Self, Item): 
         """
         Should remove the item from the store since player has it.
         """
+        Self.Store_Item.get_store.pop(Item)
     
-    def get_item(List):
-        """
-        Should return the remaining item the player can still buy from the store.
-        """
+    def deduct_amount(Self, amount):
+        Self.Coin -= amount
     
-    
-    
+    def list_item(Self):
+        #get images for all the thing in the store currently
+        Images = []
+        Costs = []
+        Dictionary = {}
+        for ID in Self.Store_Item.get_store().keys():
+            Images = Images.append(CLOTH_IMAGE(ID))
+            Costs = Costs.append(CLOTH_COST(ID))
+
+        for n in range(len(Self.Store_Item.get_store().keys())):
+            Image, Cost= Images[n], Costs[n]
+            ID = Self.Store_Item.get_store().keys()[n]
+
+            Dictionary[ID] = (Image, Cost)
+
+        return Dictionary
